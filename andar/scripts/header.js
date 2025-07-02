6632b5a61a6330204ab20c2f9643cfe94b410f68
daeun <3v3r.alp@gmail.com>
2025-07-03 00:32:09 +0900

    반응형 추가 중

--- andar/scripts/header.js
+++ andar/scripts/header.js
@@ -1,11 +1,25 @@
+//데스크톱 nav 변수
 const searchCloseBtn = document.querySelector('.search_close_btn');
 const searchBtn = document.querySelector('.search_btn1');
 const searchForm = document.querySelector('.search_bg');
 const lnbWrap = document.querySelector('.lnb_wrap');
 const gnb = document.querySelector('.gnb');
-console.log(searchBtn, searchForm);
+//모바일,태블릿 nav 변수
+const menuOpenBtn = document.querySelector('.menu_btn');
+const mobileRightMenu = document.querySelector('.mobile_menu');
+const menuCloseBtn = document.querySelector('.mobile_menu .top img');
+const menuMoreBtn = document.querySelectorAll('.menu_more_btn img');
+const mobileLnb = document.querySelectorAll('.mobile_menu .lnb');
+//console.log(searchBtn, searchForm);
+
+//기초 셋팅
 searchForm.style.display = 'none';
 lnbWrap.style.transform = 'translateY(-450px)';
+
+mobileRightMenu.style.display = 'none';
+
+
+//데스크톱 함수
 searchBtn.addEventListener('click',()=>{
     searchForm.style.display = 'block';
 });
@@ -27,3 +41,21 @@ lnbWrap.addEventListener('mouseout',()=>{
     lnbWrap.style.transform = 'translateY(-450px)';
     lnbWrap.style.transition = 'all 0.5s';
 });
+
+//모바일 함수
+menuOpenBtn.addEventListener('click',()=>{
+    mobileRightMenu.style.display = 'block';
+})
+menuCloseBtn.addEventListener('click',()=>{
+    mobileRightMenu.style.display = 'none';
+})
+menuMoreBtn.forEach((obj, ind, arr)=>{
+    mobileLnb.forEach((obj, ind, arr)=>{
+        obj.style.display = 'none';
+        menuMoreBtn[ind].addEventListener('click',()=>{
+            mobileLnb[ind].style.display = 'block';
+            menuMoreBtn[ind].src = './images/arrow_drop_up.png';
+        })
+    })
+})
+
