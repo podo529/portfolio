f7313e31b9ccb98cb4a07662bb377ea55746f976
daeun <3v3r.alp@gmail.com>
2025-07-06 17:40:41 +0900

    메인페이지 완성

--- andar/scripts/index.js
+++ andar/scripts/index.js
@@ -1,1 +1,24 @@
-const topSwiper = new Swiper('.top_banner');
+const topSwiper = new Swiper('.top_banner',{
+        pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+    },
+    navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+    },
+});
+
+
+const swiper = new Swiper(".look_book_swiper", {
+    direction: 'horizontal', // 가로 방향 슬라이드
+    slidesPerView: 'auto',   // 자동 슬라이드 너비 설정
+    spaceBetween: 20,        // 슬라이드 간 간격
+    freeMode: true,          // 드래그로 자유롭게 움직이기
+    watchOverflow: true,
+    scrollbar: {
+        el: ".swiper-scrollbar",
+        draggable: true, // 드래그 가능하도록 설정
+        hide: false      // 항상 보이게
+    },
+});
